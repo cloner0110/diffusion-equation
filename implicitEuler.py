23da701a3c15ca309d73e24ad8e82a7de720c221
AmirHossein Taran <det.em.tw@gmail.com>
2019-04-07 01:35:45 +0430

    final Version and K updated as a function of T

--- implicitEuler.py
+++ implicitEuler.py
@@ -1,29 +1,45 @@
 from scipy.sparse import diags
 import numpy as np
+from math import pi
 import matplotlib.pyplot as plt
 from scipy.sparse import diags
-def implicitEuler(dt,dy,t_max,y_max,alpha,V0,V1):
-    s = alpha*dt/dy**2  
-    y = np.arange(0,y_max+dy,dy) 
+def k_calculator(T):
+    k=T/((1.829*10**(-4))*T**(n))+0.0245
+    return k
+def implicitEuler(dt,dx,t_max,x_max,alpha):
+    s = alpha*dt/dx**2  
+    y = np.arange(0,x_max+dx,dx) 
     t = np.arange(0,t_max+dt,dt)
+    dx = 0.001
+    k=237 ; h=35 ; Acond=pi*0.005*0.005
+    Aconv=pi*2*0.005*0.1
     nt = len(t) 
     ny = len(y) 
+    V0 = 120+273 
+    Vinf=273+15
     V = np.zeros((ny,)) 
-    V[0] = V0 
-    V[-1] = V1 
     A = diags([-s, 1+2*s, -s], [-1, 0, 1], shape=(ny-2, ny-2)).toarray() 
     for n in range(nt): 
         Vn = V ;B = Vn[1:-1] 
-        B[0] = B[0]+s*V0 ;B[-1] = B[-1]+s*V1
+        B[0] = B[0]+s*Vn[0] ;B[-1] = B[-1]+s*Vn[-1]
         V[1:-1] = np.linalg.solve(A,B) 
-    return y,t,V,s
-dt = 0.01 
-dy = 0.0005 
-alpha = 2*10**(-4) 
-y_max = 0.04 # in m
-V0 = 10.0 
-V1 = 0.0 
-for time in np.linspace(0,1.0,10):
-    y,t,V,s = implicitEuler(dt,dy,time,y_max,alpha,V0,V1)
+        V[0]=120+273
+        t_ave=(Vn[-1]+Vn[-2])/2
+        #switcher for 4th part of the Exercise , disable if you want to use constant K
+        #c=dx*h*Aconv/(k_calculator(t_ave)*Acond)
+        c=dx*h*Aconv/(k*Acond)
+        V[-1]=((c*Vinf)+V[-2])/(1+c)
+    return y,V
+dt = 0.001 ;dx=0.001
+alpha = 97.1*10**(-6) 
+x_max = 0.1 
+y = np.arange(0,x_max+dx,dx) 
+nx = len(y)
+plt.figure(figsize=(7,5))
+V = np.zeros((nx,)) # initial condition
+V[:] = 273+30
+for time in np.linspace(0,100.0,5):
+    y,V = implicitEuler(dt,dx,time,x_max,alpha)
     plt.plot(y,V,'k')
 plt.show()
+
